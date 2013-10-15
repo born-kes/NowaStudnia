cc02ea157b8fcccb15725bc0f213fcdb3730ed03
bornkes <born-kes@o2.pl>
2013-10-15 18:57:09 +0200

    Komentarz / adaptowanie dla ARKUSZA STYLI

--- game.php/game.php
+++ game.php/game.php
@@ -1,10 +1,28 @@
 <?php
-$budynki = ( sql_queryArray('SELECT * FROM `budynki`') );
-$jednostki = ( sql_queryArray('SELECT * FROM `jednostki`') );
-$manu = ( sql_queryArray('SELECT * FROM `menu`') );
+
+/*
+echo sql_dopasuj_zap(
+				array(	'budynki'	=>'*'),
+				NULL,
+				array(
+				1=>'de',//Form[0]
+				0=>array('location'	=>$GET['location'].' AS name'),
+				'location'	=>'de'
+					)
+				);
+//*/
+$budynki_BD = sql_queryArray(
+				array(	'budynki'	=>'*')
+				);
+$jednostki_BD = sql_queryArray(
+				array(	'jednostki'	=>'*')
+				
+				);
+$manu_BD = ( sql_queryArray('SELECT * FROM `menu`') );
 
 
 define('plemiona',"../plemiona/");
+define('img',plemiona."img/");
 head('title','Kalkulator do Plemion');
 head('meta','text/html; charset=UTF-8');
 head('css',		plemiona.'css/game.css?1');
@@ -25,7 +43,7 @@ body('kalkulator_game');
 # print_r($manu);
 
 function jednostki($index,$nag³owek = true){
-global $jednostki,$budynki;
+global $jednostki_BD,$budynki_BD;
 /* sprawdz które jednostki mam wstawiæ 
  * 
  */
@@ -33,22 +51,22 @@ global $jednostki,$budynki;
 if($nag³owek)
 	$str='
 	<table><tbody><tr>
-				<th >Jednostka</th>
-		<th colspan="4">Zapotrzebowanie</th>
-		<th width="100" class="nowrap">Czas  (gg:mm:ss)</th>
-		<th class="nowrap">W wiosce/ogólnie</th>
-				<th>Rekrutacja</th>
+				<th >'.t('Jednostka').'</th>
+		<th colspan="4">'.t('Zapotrzebowanie').'</th>
+		<th width="100" class="nowrap">'.t('Czas').'  (gg:mm:ss)</th>
+		<th class="nowrap">'.t('W wiosce').'/'.t('ogólnie').'</th>
+				<th>'.t('Rekrutacja').'</th>
 	</tr>';
 	
-$str.='<tr id="'.$budynki[$index]['name'].'_fin" class="nowrap"><td></td><tr>';
+$str.='<tr id="'.$budynki_BD[$index]['de'].'_fin" class="nowrap"><td></td><tr>';
 														
-	foreach($jednostki as $r){
+	foreach($jednostki_BD as $r){
 	if($r['budynek']==$index)
 $str.='<tr class="row_a">
 			<td class="nowrap">
 				<a onclick="return false" class="unit_link" href="#">
-				<img class="" alt="" title="" src="img/unit_'.$r['img'].'.png">
-'.$r['name'].'				
+				<img class="" alt="" title="" src="'.img.'unit_'.$r['de'].'.png">
+'.t($r['de']).'				
 				</a>
 			</td>
 			<td class="nowrap"><span class="icon header wood"> </span> 50</td>
@@ -59,54 +77,69 @@ $str.='<tr class="row_a">
 
 			<td>0/0</td>
 							<td class="nowrap">
-				<input type="text" tabindex="1" maxlength="5" style="width: 50px; color: black;" id="'.$r['skrot'].'" class="recruit_unit" name="'.$r['name'].'" onchange="wojsko()" >
-								<a href="javascript:unit_build_block.set_max(\''.$r['name'].'\')" id="'.$r['name'].'_0_a">(1200)</a>
+				<input type="text" tabindex="1" maxlength="5" style="width: 50px; color: black;" id="'.$r['de'].'" class="recruit_unit" name="'.$r['de'].'" onchange="wojsko()" >
+								<a href="javascript:unit_build_block.set_max(\''.$r['de'].'\')" id="'.$r['de'].'_0_a">(1200)</a>
 			</td>
 		
 		</tr>';
 }
-	return $str.='</tbody></table></div>';
-
+if($nag³owek)
+	 $str.='</tbody></table>';
+return $str;
 }
-function budy($arry=null,$index=1){
+function budy($arry=null,$index=1){$fin='';
 if( is_array($arry))
 	foreach($arry as $name){}
 	
-global $budynki;
-$ratusz='<table class="vis nowrap"><tbody>
-<tr>
-			<th>Budynki</th>
-			<th colspan="4">Zapotrzebowanie</th>
-			<th width="100">Czas budowy <img width="13" height="13" src="img/questionmark.png" class="tooltip" alt=""></th>
-			<th style="width:100px;">Wybuduj</th>
-			<th style="width:100px;">Burzenie</th>
-		</tr>';
-foreach($budynki as $r){
-$ratusz.='<tr id="buildrow_'.$r['id'].'">
-			  <td><input type="text" onchange="suma();" size="1" value="1" id="'.strtolower($r['name']).'">
-				<a href="game.php?village=61342&amp;screen='.$r['name'].'" onclick="$(\'#'.$r['name'].'_the\').toggle(); return false">
-				<img alt="" title="'.$r['name'].'" src="img/'.$r['img'].'"> '.$r['name'].'
-				</a>
-				<span class="nowrap"></span><span id="'.strtolower($r['name']).'_s" class="nowrap"></span>
-			  </td>
-			  <td><span class="icon header wood"> </span></span>1008</span></td>
-			  <td><span class="icon header stone"> </span></span>957</span></td>
-			  <td><span class="icon header iron"> </span></span>454</span></td>
-			  <td><span class="icon header population"> </span></span>3</span></td>
-			  <td>0:15:03</td>
-			  <td style="white-space: normal;">
-				 <a onclick="rozbuduj(\''.strtolower($r['name']).'\');return false;" href="game.php?village=61342&amp;action=upgrade_building&amp;h=7db0&amp;id=barracks&amp;type=main&amp;screen=main" id="main_buildlink_'.$r['id'].'" class="nowrap">Rozbuduj</a>
-			  <td style="white-space: normal;">
+global $budynki_BD,$ratusz;
 
-				 <a onclick="return BuildingMain.build(\'barracks\')" href="game.php?village=61342&amp;action=upgrade_building&amp;h=7db0&amp;id=barracks&amp;type=main&amp;screen=main" id="main_buildlink_'.$r['id'].'" class="nowrap">wyburz</a>
-			  </td>
-			</tr>';
-			if(isset($r['function']) && $r['function']!=NULL && function_exists($r['function'])){
-			$ratusz.=' <tr><td colspan="8" id="'. $r['name'] .'_the">'.$r['function']($r['id']).'</td></tr>';
-			}
+	if(isset($ratusz['nagluwek']) )
+$fin.=	strtr($ratusz['nagluwek'] , array('{img}'=> img,) );
+foreach($budynki_BD as $r){
+	if(isset($ratusz['content']) )
+$fin.=	strtr($ratusz['content'] , array(	
+											'{img}'=> img ,
+											'{de}' => $r['de'],
+											'{smol_de}'=> strtolower($r['de'])
+											)
+				);
+					
+	if(isset($r['function']) && $r['function']!=NULL && function_exists($r['function']))
+$fin.=	strtr($ratusz['function'] ,array('{function}'=> $r['function']($r['id'])) );
+
 $index++;
 }
-	$ratusz.='</tbody></table>';
-return $ratusz;
+$fin.=	$ratusz['stopka'];
+return $fin;
 
   }
+
+function plac($index,$nag³owek = true){
+global $jednostki_BD,$plac; $linia=0;$strs=array('','','');
+/* sprawdz które jednostki mam wstawiæ 
+ * 
+ */
+$myk=1;
+
+//$str.='<tr id="'.$budynki[$index]['de'].'_fin" class="nowrap"><td></td><tr>';
+		//<tr class="row_a">												
+	foreach($jednostki_BD as $r){
+if($myk!=$r['budynek']){$linia=0;$myk=$r['budynek'];}
+	
+$strs[$linia++] .=strtr($plac['content'] , array(	
+											'{img}'=> img ,
+											'{de}' => $r['de']
+											)
+				);
+
+}
+if($nag³owek)
+	$str=$plac['nagluwek'];
+	foreach($strs as $r){
+	$str.='<tr class="row_a">'.$r.'</tr>';
+	}
+	 $str.=$plac['content2'];
+if($nag³owek)
+	 $str.=$plac['stopka'];
+return $str;
+}
